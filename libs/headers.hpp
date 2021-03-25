1e53a1a82e1a4d38cbf9eff81ae043c241a236e6
cpetryka <cezaryp10@gmail.com>
2021-03-25 20:32:44 +0100

    Add headers.hpp

---
+++ libs/headers.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Cezary Petryka on 25.03.2021.
+//
+
+#ifndef ZAD_2_HEADERS_HPP
+#define ZAD_2_HEADERS_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#endif //ZAD_2_HEADERS_HPP
